a6e911a0ae31d1be83360677d42a177d211a1fe9
koumanwei <874899973@qq.com>
2017-04-24 11:43:06 +0800

    增加登陆页面

---
+++ app/src/main/java/com/example/chen/oamanager/ui/main/activity/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.chen.oamanager.ui.main.activity;
+
+import com.example.chen.oamanager.R;
+import com.jaydenxiao.common.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
